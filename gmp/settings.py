28eee13c9bf82808f0ca1cb7aa8fb0caf38214b4
imeed88 <45313695+imeed88@users.noreply.github.com>
2023-09-18 11:58:48 +0100

    Update settings.py

--- gmp/settings.py
+++ gmp/settings.py
@@ -53,7 +53,8 @@ MIDDLEWARE = [
 ]
 
 
-CSRF_TRUSTED_ORIGINS = ['https://yppv-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ["https://yppv-production.up.railway.app",
+                       "http://yppv-production.up.railway.app",]
 
 
 ROOT_URLCONF = 'gmp.urls'
